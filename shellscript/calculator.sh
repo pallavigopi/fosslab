cf213c3b45bdca8833bfa0a669999dc858f34a92
Pallavi V G <pallavivg98@gmail.com>
2018-04-03 13:48:48 +0530

    calculator shellscript

---
+++ shellscript/calculator.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+clear
+sum=0
+i="y"
+
+echo "Enter first number:"
+read n1
+echo "Enter second number:"
+read n2
+while [ $i = "y" ]
+do
+echo
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+echo "5. Modulus"
+echo "Enter your choice:"
+read ch
+case $ch in
+  1)sum=`expr $n1 + $n2`
+    echo "Sum ="$sum;;
+  2)sum=`expr $n1 - $n2`
+    echo "Sub ="$sum;;
+  3)sum=`expr $n1 \* $n2`
+    echo "Mul ="$sum;;
+  4)sum=`expr $n1 / $n2`
+    echo "Div ="$sum;;
+  5)sum=`expr $n1 % $n2`
+    echo "Mod ="$sum;;
+  *)echo "Invalid choice";;
+esac
+echo "Do you want to continue ?"
+read i
+if [ $i != "y" ]
+then
+   exit
+fi
+done
