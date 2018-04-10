740bb667696e45516a6da8ae11edbb585e86ced1
Pallavi V G <pallavivg98@gmail.com>
2018-04-10 19:50:38 +0530

    Basic php form added(more in read me)

---
+++ misc/lampstack/form/welcome.php
@@ -0,0 +1,17 @@
+<html>
+ <link rel="stylesheet" href="style.css">
+    <body>
+	<div class="grid">
+	<div id="cards" class="container">
+	<div class="card box">
+		<h1>YOUR RESPONSE</h1>
+	<hr>
+        <p><b>Name:</b><?php echo $_POST["name"]; ?><p><br>
+	<p><b>Email:</b><?php echo $_POST["email"]; ?><p><br>
+	<p><b>Age:</b><?php echo $_POST["age"]; ?><p><br>
+	<p><b>Job:</b><?php echo $_POST["job"]; ?><p><br>
+	</div>
+	</div>
+	</div>
+    </body>
+</html>
